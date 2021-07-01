0040987fb42528ad4ac50aa0e021a9721e4a6c2a
Uttam Patro <uttampatro890@gmail.com>
2021-07-01 13:12:24 +0530

    options

--- src/entity/Quiz.ts
+++ src/entity/Quiz.ts
@@ -1,28 +1,28 @@
 import {
-  BaseEntity,
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from "typeorm";
-import { QuestionSet } from "./QuestionSet";
+    BaseEntity,
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { QuestionSet } from './QuestionSet';
 
-@Entity("quiz")
+@Entity('quiz')
 export class Quiz extends BaseEntity {
-  @PrimaryGeneratedColumn("uuid")
-  id: number;
+    @PrimaryGeneratedColumn('uuid')
+    id: number;
 
-  @Column()
-  question: string;
+    @Column()
+    question: string;
 
-  @Column()
-  answer: string;
+    @Column()
+    answer: string;
 
-  @Column()
-  options: string;
+    @Column('text', { array: true, default: '{}' })
+    options: string[];
 
-  @ManyToOne(() => QuestionSet, (questionSet) => questionSet.quiz)
-  @JoinColumn()
-  questionSet: QuestionSet;
+    @ManyToOne(() => QuestionSet, questionSet => questionSet.quiz)
+    @JoinColumn()
+    questionSet: QuestionSet;
 }
